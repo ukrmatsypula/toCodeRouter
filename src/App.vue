8a492fbe6cde4efe8294d9a7b614cd15b9830152
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-14 21:42:44 +0200

    render router-link in our app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="app">
-    <header><h1>header</h1></header>
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">VUE-CLI</div>
+            <ul class="navbar-list">
+              <li
+                class="navbar-item"
+                v-for="(link, index) in links"
+                :key="index"
+              >
+                <router-link class="navbar-link" :to="link.url">
+                  {{ link.title }}
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
     <router-view></router-view>
+    <footer>footer</footer>
   </div>
 </template>
 
@@ -9,6 +29,12 @@
 export default {
   name: "App",
   components: {},
+  data: () => ({
+    links: [
+      { title: "Home", url: "/" },
+      { title: "Example", url: "/example" },
+    ],
+  }),
 };
 </script>
 
